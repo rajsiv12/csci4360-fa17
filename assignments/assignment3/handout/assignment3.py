fde13999ec5a3b9fbc43f0d5b0e64c003b7c37c3
rajsiv12 <orcsy@s172-20-59-h1.paws.uga.edu>
2017-10-15 14:20:59 -0400

    commit

--- assignments/assignment3/handout/assignment3.py
+++ assignments/assignment3/handout/assignment3.py
@@ -1,6 +1,13 @@
 import argparse
 import numpy as np
 from scipy.linalg import pinv, svd # Your only additional allowed imports!
+# import matplotlib.pyplot as plt
+
+class LDS:
+    def __init__(self):
+        pass
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Assignment 3",
@@ -23,5 +30,26 @@ if __name__ == "__main__":
 
     # Read in the dynamic texture data.
     M = np.load(input_file)
+    f, h, w = M.shape
+    M = M.reshape((M.shape[0],-1))
+    M = M.T
+    # print(M.shape)
+    U, s, V = svd(M)
+    # print(U.shape, s.shape, V.shape)
+    U = U[:,0:q]
+    s = np.diag(s[0:q])
+    V = V[0:q,:]
+    # print(U.shape, s.shape, V.shape)
 
+
+    A = np.matmul(np.matmul(s,V[:, 0:f-2]),pinv(np.matmul(s,V[:,1:f-1])))
+    # print(A.shape)
+    X_1_tp1 =np.matmul(A,np.matmul(s,V))
+    X_tp1 = X_1_tp1[:,-1]
+
+    y = np.matmul(U,X_tp1).reshape(h,w)
+    np.save(output_file, y)
+    # print(y.shape)
+    # plt.imshow(y)
+    # plt.show()
     ### FINISH ME
